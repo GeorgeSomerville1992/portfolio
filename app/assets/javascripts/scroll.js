4cfb7f1429205feb3e872394856361bda94aeb6f
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-07-26 09:53:20 +0100

    dev not pullign through

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -9,6 +9,7 @@
 $(function() {
     var y=0;
     $("#Project").click(function(e){
+      console.log("go!")
         e.preventDefault();
         $(".maincontentcontainer").animate({scrollTop: y+=800}, 1000);
     }) 
