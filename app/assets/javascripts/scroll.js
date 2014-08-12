964391e46aa593ad31ec57fb51dc8750937b3728
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-08-12 12:58:08 +0100

    change

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -8,9 +8,14 @@
 
 $(function() {
     var y=0;
-    $("#Project").click(function(e){
-      console.log("go!")
+    $("#Projectt").click(function(e){
+      console.log("go!");
         e.preventDefault();
         $(".maincontentcontainer").animate({scrollTop: y+=800}, 1000);
     }) 
 })
+
+
+// $(document).ready(function() {
+//    $('#Projectt').localScroll({duration:800});
+// });
